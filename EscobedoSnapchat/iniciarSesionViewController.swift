f5adc13bedb68337308f2291da02b06a8c212ab1
DiegoEscobedo17 <diego.escobedo@tecsup.edu.pe>
2022-10-31 14:08:55 -0500

    Añadiendo Firebase

---
+++ EscobedoSnapchat/iniciarSesionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  EscobedoSnapchat
+//
+//  Created by Diego Escobedo on 10/31/22.
+//  Copyright © 2022 Diego. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseCore
+
+class iniciarSesionViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in print("Intentando Iniciar Sesion")
+            if error != nil{
+                print("Se presento el siguiente error: \(error)")
+            }else{
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    
+
+}
+
