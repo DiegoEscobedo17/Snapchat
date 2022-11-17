ef00484d1752a591ce06550da1c18d0b2fda6070
DiegoEscobedo17 <diego.escobedo@tecsup.edu.pe>
2022-11-17 11:28:47 -0500

    Mejoras

---
+++ EscobedoSnapchat/Modelos/Snap.swift
@@ -0,0 +1,16 @@
+//
+//  Snap.swift
+//  EscobedoSnapchat
+//
+//  Created by Diego Escobedo on 11/14/22.
+//  Copyright © 2022 Diego. All rights reserved.
+//
+
+import Foundation
+class Snap {
+    var imagenURL = ""
+    var descrip = ""
+    var from = ""
+    var id = ""
+    var imagenID = ""
+}
--- EscobedoSnapchat/ViewControllers/ImagenViewController.swift
+++ EscobedoSnapchat/ViewControllers/ImagenViewController.swift
@@ -13,26 +13,45 @@ class ImagenViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     var imagePicker = UIImagePickerController()
     
-    @IBAction func camaraTapped(_ sender: Any) {
+    @IBAction func mediaTapped(_ sender: Any) {
         imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
+    @IBAction func camaraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
         present(imagePicker, animated: true, completion: nil)
     }
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var descripcionTextField: UITextField!
     @IBOutlet weak var elegirContactoBoton: UIButton!
+    
+    var imagenID = NSUUID().uuidString
     @IBAction func elegirContactoTapped(_ sender: Any) {
         self.elegirContactoBoton.isEnabled = false
         let imagenesFolder = Storage.storage().reference().child("imagenes")
         let imagenData = imageView.image?.jpegData(compressionQuality: 0.50)
-        let cargarImagen = imagenesFolder.child("\(NSUUID().uuidString).jpg").putData(imagenData!, metadata: nil) { (metadata, error) in if error != nil{
-            self.mostrarAlerta(titulo: "Error", mensaje: "Se produjo un error al subir la imagen. Verifique su conexion a internet y vuelva a intentarlo", accion: "Aceptar")
-            self.elegirContactoBoton.isEnabled = true
-            print("Ocurrio un error al subir imagen: \(error) ")
-        }else{
-            self.performSegue(withIdentifier: "seleccionarContactoSegue", sender: nil)
-            }
+        let cargarImagen = imagenesFolder.child("\(imagenID).jpg")
+            cargarImagen.putData(imagenData!, metadata: nil) { (metadata, error) in
+                if error != nil{
+                    self.mostrarAlerta(titulo: "Error", mensaje: "Se produjo un error al subir la imagen. Verifique su conexion a internet y vuelva a intentarlo", accion: "Aceptar")
+                    self.elegirContactoBoton.isEnabled = true
+                    print("Ocurrio un error al subir imagen: \(error) ")
+                    return
+                }else{
+                    cargarImagen.downloadURL(completion: {(url, error) in
+                        guard let enlaceURL = url else{
+                            self.mostrarAlerta(titulo: "Error", mensaje: "se produjo un error al obtener informacion de imagen.", accion: "Cancelar")
+                            self.elegirContactoBoton.isEnabled = true
+                            print("Ocurrio un error al obtener informacion de imagen: \(error) ")
+                            return
+                        }
+                        self.performSegue(withIdentifier: "seleccionarContactoSegue", sender: url?.absoluteString)
+                    })
+                    }
         }
+        /*
         let alertaCarga = UIAlertController(title: "Cargando Imagen ...", message: "0%", preferredStyle: .alert)
         let progresoCarga : UIProgressView = UIProgressView(progressViewStyle: .default)
         cargarImagen.observe(.progress) { (snapshot) in let porcentaje = Double(snapshot.progress!.totalUnitCount)
@@ -48,6 +67,7 @@ class ImagenViewController: UIViewController, UIImagePickerControllerDelegate, U
         alertaCarga.addAction(btnOK)
         alertaCarga.view.addSubview(progresoCarga)
         present(alertaCarga, animated: true, completion: nil)
+        */
     }
     
     override func viewDidLoad() {
@@ -65,6 +85,10 @@ class ImagenViewController: UIViewController, UIImagePickerControllerDelegate, U
         imagePicker.dismiss(animated: true, completion: nil)
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let siguienteVC = segue.destination as! ElegirUsuarioViewController
+        siguienteVC.imagenURL = sender as! String
+        siguienteVC.descrip = descripcionTextField.text!
+        siguienteVC.imagenID = imagenID
         
     }
     func mostrarAlerta(titulo: String, mensaje: String, accion: String) {
